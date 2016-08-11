a76cc4a65986ed9861ca2ad4032638b7f8f9e9da
Stanislav Seletskiy <s.seletskiy@gmail.com>
2016-08-11 16:58:08 +0700

    correct work via x11 forwarding

--- too-long.plugin.zsh
+++ too-long.plugin.zsh
@@ -1,6 +1,12 @@
 autoload -U add-zsh-hook
 zmodload zsh/datetime
 
+_zsh_too_long_pipe="$( {
+    mkfifo $(
+        mktemp -tu zsh-too-long-XXXXXX | tee /dev/stderr
+    ) } 2>&1
+)"
+
 _zsh_too_long_callback() {
     local executed_command=$1
     local exit_code=$2
@@ -10,27 +16,28 @@ _zsh_too_long_callback() {
         title="[failure] non-zero exit code: $exit_code"
     fi
 
-    notify-send "$title" -- " $executed_command"
+    # dbus-launch to show notification on the remote client,
+    # e.g. connected via x11 forwarding
+    dbus-launch notify-send "$title" -- " $executed_command"
 }
 
 _zsh_too_long_start() {
     _zsh_too_long_start_time=$EPOCHSECONDS
-    _zsh_too_long_window_id=$(xdotool getwindowfocus getwindowpid)
+
+    # run in the subshell to avoid slow xdotool run time
+    (
+        local pid="$(xdotool getwindowfocus getwindowpid)"
+
+        echo "$pid" > "$_zsh_too_long_pipe"
+    ) &|
+
     _zsh_too_long_executing_command="$1"
 }
-
-_zsh_too_long_start
 
 _zsh_too_long_stop() {
     local command_exit_code=$?
 
     if ! [ "$_zsh_too_long_executing_command" ]; then
-        return
-    fi
-
-    local current_window_id=$(xdotool getwindowfocus getwindowpid)
-
-    if [ "$_zsh_too_long_window_id" = "$current_window_id" ]; then
         return
     fi
 
@@ -40,12 +47,25 @@ _zsh_too_long_stop() {
     execution_time=$(($EPOCHSECONDS - $_zsh_too_long_start_time))
 
     if (( $execution_time > $threshold )); then
+        local current_window_id=$(xdotool getwindowfocus getwindowpid)
+
+        if [ "$(cat "$_zsh_too_long_pipe")" = "$current_window_id" ]; then
+            return
+        fi
+
         _zsh_too_long_callback \
             "$_zsh_too_long_executing_command" "$command_exit_code"
+    else
+        ( cat "$_zsh_too_long_pipe" > /dev/null ) &|
     fi
 
     _zsh_too_long_executing_command=""
 }
 
+_zsh_too_long_cleanup() {
+    rm "$_zsh_too_long_pipe"
+}
+
 add-zsh-hook preexec _zsh_too_long_start
 add-zsh-hook precmd _zsh_too_long_stop
+add-zsh-hook zshexit _zsh_too_long_cleanup
